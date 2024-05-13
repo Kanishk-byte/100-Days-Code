1d139eb8736dbf51fcace229c516dc97331d0230
Kanishk Khandelwal <khandelwalkanishk07@gmail.com>
2024-05-14 00:05:32 +0530

    Files

---
+++ Module 3 (Fundamentals of Programming - 2)/Pattern Printing - 2 (3)/Assingment/4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// * Ques : Print the given pattern
+// A B C D E F G
+// A B C   E F G
+// A B       F G
+// A           G
+
+int main()
+{
+
+    int n;
+    cout<<"No. of lines : ";
+    cin>>n;
+
+    return 0;
+}
