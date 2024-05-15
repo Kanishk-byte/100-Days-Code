54e32e3a57c2a8a926d5125b16dd1d40d814d99d
Kanishk Khandelwal <khandelwalkanishk07@gmail.com>
2024-05-15 21:08:00 +0530

    Files

---
+++ demo.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// * Ques :
+
+int main()
+{
+
+    string a = "abcd";
+    string b = "ef";
+
+    char c1 = a[0];
+    char c2 = a[1];
+    char c3 = a[2];
+    char c4 = a[3];
+
+    char c5 = b[0];
+    char c6 = b[1];
+
+    cout << c5 << c2 << c3 << c4 << " " << c1 << c6;
+
+    return 0;
+}
