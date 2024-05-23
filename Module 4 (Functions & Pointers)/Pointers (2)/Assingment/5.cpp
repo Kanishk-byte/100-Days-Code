20bfd3a93024792d0649d5e7518943c46b2bf794
Kanishk Khandelwal <khandelwalkanishk07@gmail.com>
2024-05-23 19:18:48 +0530

    Files

---
+++ Module 4 (Functions & Pointers)/Pointers (2)/Assingment/5.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// * Ques : Is the following program snippet correct?
+
+int main()
+{
+
+    int a = 10, b = 20;
+    int *ptr;
+    *ptr = 5;
+
+    cout<<"No, the statement is not correct.";
+
+    return 0;
+}
