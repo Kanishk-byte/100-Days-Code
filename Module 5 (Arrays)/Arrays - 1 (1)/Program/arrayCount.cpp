ebaf5e5c317f020f9bf9b1f3e841ebec3875d487
Kanishk Khandelwal <khandelwalkanishk07@gmail.com>
2024-05-27 07:15:49 +0530

    Files

--- Module 5 (Arrays)/Arrays - 1 (1)/Program/arrayCount.cpp
+++ Module 5 (Arrays)/Arrays - 1 (1)/Program/arrayCount.cpp
@@ -20,7 +20,7 @@ int main()
 
     // * Input
     cout << "Enter elements of Array : " << endl;
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size - 1; i++)
     {
         cin >> arr[i];
     }
